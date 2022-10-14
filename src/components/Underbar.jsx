1fc4a833901a147f0ead46b5fee49e6a209f96c4
ayberk1414 <68445662+ayberk1414@users.noreply.github.com>
2022-10-14 18:21:33 +0300

    udemy0

---
+++ src/components/Underbar.jsx
@@ -0,0 +1,10 @@
+import '../index.css'
+import { AiOutlineHeart, AiOutlineShoppingCart, AiOutlineBell } from 'react-icons/ai'
+
+export default function Underbar() {
+    return (
+        <div className='bg-white h-[32px] w-full flex p-5 gap-4 place-items-center  whitespace-nowrap'>
+            <p>Ddddd</p>
+        </div>
+    )
+}
