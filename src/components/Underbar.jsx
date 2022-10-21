6468be957a601df53e07e5f7d873f7c1badaac75
ayberk1414 <68445662+ayberk1414@users.noreply.github.com>
2022-10-21 12:11:48 +0300

    udemy clone

--- src/components/Underbar.jsx
+++ src/components/Underbar.jsx
@@ -3,8 +3,19 @@ import { AiOutlineHeart, AiOutlineShoppingCart, AiOutlineBell } from 'react-icon
 
 export default function Underbar() {
     return (
-        <div className='bg-white h-[32px] w-full flex p-5 gap-4 place-items-center  whitespace-nowrap'>
-            <p>Ddddd</p>
+        <div className=' bg-white h-[72px] flex justify-between items-center h-12 px-4  '>
+            <ul className='flex flex-between '>
+                <li className="pl-40"> Yazilim Geliştirme</li>
+                <li className=''> İşletme</li>
+                <li className=' '> Finans ve Muhasebe</li>
+                <li className=''> Bt ve Yazılım</li>
+                <li className=''> Ofiste Verimlilik</li>
+                <li className=' '> Kisisel Gelişim</li>
+                <li className=''> Tasarım</li>
+                <li className=''> Pazarlama</li>
+                <li className=''> Sağlik ve Fitness</li>
+                <li className='pr-20'> Müzik</li>
+            </ul>
         </div>
     )
 }
